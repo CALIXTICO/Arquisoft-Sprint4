9d831373749a945ad3aace34f5739ed5faaa689c
Alejandr2106 <a.gonzalez48@uniandes.edu.co>
2023-12-05 23:58:53 -0500

    .
    .

---
+++ pacientes/src/main/java/com/example/demo/HistoriaClinica.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class HistoriaClinica {
+
+    @Id
+    private String id;
+    private String pacienteNombre;
+    private String medicoNombre;
+    private String diagnostico;
+
+
+    public HistoriaClinica() {
+    }
+
+    // Constructor
+    public HistoriaClinica(String pacienteNombre, String medicoNombre, String diagnostico) {
+        this.pacienteNombre = pacienteNombre;
+        this.medicoNombre = medicoNombre;
+        this.diagnostico = diagnostico;
+    }
+
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPacienteNombre() {
+        return pacienteNombre;
+    }
+
+    public void setPacienteNombre(String pacienteNombre) {
+        this.pacienteNombre = pacienteNombre;
+    }
+
+    public String getMedicoNombre() {
+        return medicoNombre;
+    }
+
+    public void setMedicoNombre(String medicoNombre) {
+        this.medicoNombre = medicoNombre;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+}
